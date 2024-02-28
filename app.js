6e460721434d76675ef36c8322d6a9b890e832e8
haashirmemon <haashirmemon6@gmail.com>
2024-02-28 05:58:17 -0800

    javascript topic

---
+++ app.js
@@ -0,0 +1,85 @@
+// var username = ("haashir shakeel ahmed")
+
+
+// document.write(username)
+//  var num1= 11
+//  if (num1 > 12) {
+//     console.log("ifchalnachahiyai")
+    
+//  } else {
+//     console.log("warnabekaarhai")}
+
+
+//  var bankbalance=+prompt("enter your bank balance")
+// if (bankbalance > 500000) {
+//     console.log("bhaijaan mubarak ho rishta pakka")
+    
+// } else {
+//     console.log("yaar maazrat abba amma nahi maan rahai  ")
+// }
+
+// if(num > 5){
+//     console.log('if')
+// }
+// else if(num > 6){
+//     console.log('else if')
+// }
+// else{
+//     console.log('else')
+// }
+
+
+// var percentage= +prompt("enter your percentage!");
+
+
+// if (percentage > 100) {
+//     console.log("bhai jaan tum wargaye")
+// }  else if(percentage >= 90){
+//     console.log("maalik mubarak ho A5 grade")
+// }else if(percentage >= 100){
+//     console.log("maalik mubarak ho A1 grade")
+// } else if(percentage >= 70  &percentage <=80){
+//     console.log("maalik mubarak ho A grade")
+// } else if(percentage >= 50 &percentage <= 60){
+//     console.log("maalik mubarak ho c grade")
+// } else if(percentage >= 60& percentage<=70){
+//     console.log("maalik mubarak ho B grade")
+// }
+// else{
+//     console.log("percentage")
+// }
+    
+
+// var percentage = +prompt('enter your percentage!');
+
+// if (percentage > 100) {
+//     console.log('bhai percent to deda sahi!');
+// } else if (percentage < 50) {
+//     console.log('maalik rickshaw chalao!')
+// } else if (percentage >= 50 && percentage <= 60) {
+//     console.log('C grade aya haa maalik.')
+// } else if(percentage >=61 && percentage <= 70){
+//     console.log('B grade aya ha maalik')
+// }
+// else {
+//     console.log(percentage);
+// }
+
+// var num1= 
+// if (num1>8) {
+//     console.log("8 sai barahai")
+    
+//  }if (num1>6) {
+//     console.log("6 sai barahai")
+    
+//  }if (num1>2) {
+//     console.log("2 sai barahai")
+    
+//  } else {
+//     console.log("2 sai chota hai")
+    
+//  }
+
+
+
+
